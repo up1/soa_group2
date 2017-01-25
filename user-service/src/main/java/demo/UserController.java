a01d7d9d48d84da4bafad3e0a454eddafe376f91
Leconize <g.supavit@gmail.com>
2017-01-25 16:40:32 +0700

    homework query users

---
+++ user-service/src/main/java/demo/UserController.java
@@ -0,0 +1,31 @@
+package demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class UserController {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @RequestMapping("/user")
+    public User getUser(@RequestParam(value="id", defaultValue="1") int id) {
+        return this.userRepository.findById((long) id);
+    }
+
+    @RequestMapping("/users")
+    public List<User> getUsers(
+            @RequestParam(value="page", defaultValue="1") int page,
+            @RequestParam(value="item_per_page", defaultValue="10") int itemPerPage) {
+        return this.userRepository.queryUsers(page, itemPerPage);
+    }
+}
