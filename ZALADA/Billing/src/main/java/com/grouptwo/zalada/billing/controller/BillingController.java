74c27041a27d2da5d60f2ef339d8edd25b0f7cee
Leconize <g.supavit@gmail.com>
2017-03-09 21:44:06 +0700

    add two endpoints in bill service

--- ZALADA/Billing/src/main/java/com/grouptwo/zalada/billing/controller/BillingController.java
+++ ZALADA/Billing/src/main/java/com/grouptwo/zalada/billing/controller/BillingController.java
@@ -1,8 +1,12 @@
 package com.grouptwo.zalada.billing.controller;
 
+import com.grouptwo.zalada.billing.domain.Bill;
 import com.grouptwo.zalada.billing.domain.Product;
 import com.grouptwo.zalada.billing.repository.BillingRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,5 +21,23 @@ public class BillingController {
     @RequestMapping(value = "/billing", method = RequestMethod.GET)
     public String getString(){
         return "";
+    }
+
+    @RequestMapping(value = "billing/unpaid", method = RequestMethod.POST)
+    public ResponseEntity<String> createNewBill(@RequestBody Bill bill){
+        bill.setBillStatus(0);
+        billingRepository.insert(bill);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "billing/pay", method = RequestMethod.PATCH)
+    public ResponseEntity<String> updateBillStatus(@RequestBody Bill bill){
+        if(bill.getBillStatus() == null && bill.getId() != null){
+            billingRepository.updateStatus(bill);
+            return new ResponseEntity<>(HttpStatus.OK);
+        }
+        else{
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
     }
 }
--- ZALADA/Billing/src/main/java/com/grouptwo/zalada/billing/repository/BillingRepository.java
+++ ZALADA/Billing/src/main/java/com/grouptwo/zalada/billing/repository/BillingRepository.java
@@ -1,9 +1,14 @@
 package com.grouptwo.zalada.billing.repository;
 
+import com.grouptwo.zalada.billing.domain.Bill;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.client.RestTemplate;
+
+import static org.springframework.data.mongodb.core.query.Criteria.where;
 
 @Repository
 public class BillingRepository {
@@ -13,4 +18,30 @@ public class BillingRepository {
 
     @Autowired
     RestTemplate restTemplate;
+
+    public void insert(Bill bill){
+        Long timestamp = getTimeStamp();
+        bill.setBuyDate(timestamp);
+        mongoTemplate.insert(bill);
+    }
+
+
+    public void updateStatus(Bill bill){
+        Query query = new Query(where("id").is(bill.getId()));
+        int billStatus = bill.getBillStatus();
+        Update update = new Update().set("billStatus", billStatus);
+        if(isPaidStatus(billStatus)){
+            update.set("paidDate", getTimeStamp());
+        }
+        mongoTemplate.updateFirst(query, update, Bill.class);
+    }
+
+    private boolean isPaidStatus(int statusCode){
+        return statusCode == 1;
+    }
+
+    private Long getTimeStamp(){
+        return System.currentTimeMillis() / 1000L;
+    }
+
 }
