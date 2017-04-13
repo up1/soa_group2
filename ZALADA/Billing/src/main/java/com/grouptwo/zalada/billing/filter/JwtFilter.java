8965b0984f51c25846ec237633afde6bec18f231
kanathip sae-pae <s7070013@kmitl.ac.th>
2017-04-13 12:39:34 +0700

    Add Security Configuration

---
+++ ZALADA/Billing/src/main/java/com/grouptwo/zalada/billing/filter/JwtFilter.java
@@ -0,0 +1,33 @@
+package com.grouptwo.zalada.billing.filter;
+
+
+import com.grouptwo.zalada.billing.JwtBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.filter.GenericFilterBean;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+public class JwtFilter extends GenericFilterBean {
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+
+        Authentication authentication = JwtBuilder.build((HttpServletRequest) servletRequest);
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    public static void registerFilter(HttpSecurity http) {
+        http.addFilterBefore(new JwtFilter(), UsernamePasswordAuthenticationFilter.class);
+    }
+
+}
