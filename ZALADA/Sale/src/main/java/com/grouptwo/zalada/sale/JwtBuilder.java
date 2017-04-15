230fb654b57f919e877b0a17a8e2db0052155863
kanathip sae-pae <s7070013@kmitl.ac.th>
2017-04-15 18:36:12 +0700

    Add Security Configuration

---
+++ ZALADA/Sale/src/main/java/com/grouptwo/zalada/sale/JwtBuilder.java
@@ -0,0 +1,36 @@
+package com.grouptwo.zalada.sale;
+
+import com.grouptwo.zalada.sale.domain.Authenticated;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+import org.springframework.security.core.Authentication;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class JwtBuilder {
+
+    public static String secretKey = "pOnAm2017";
+    public static Long expirationTime = 600000L;
+
+    public static Authentication build(HttpServletRequest request) {
+        String token = request.getHeader("Authorization");
+
+        if(token != null) {
+            Jws<Claims> claims = Jwts.parser()
+                    .setSigningKey(secretKey)
+                    .parseClaimsJws(token.replace("Bearer" + " ", ""));
+
+            Claims body = claims.getBody();
+
+            String username = body.getSubject();
+
+            if(username != null) {
+                return new Authenticated(username);
+            }
+        }
+
+        return null;
+    }
+
+}
---
+++ ZALADA/Sale/src/main/java/com/grouptwo/zalada/sale/config/SecurityConfig.java
@@ -0,0 +1,31 @@
+package com.grouptwo.zalada.sale.config;
+
+import com.grouptwo.zalada.sale.filter.JwtFilter;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+
+@Configuration
+@EnableWebSecurity(debug = true)
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.csrf().disable();
+
+
+        http.authorizeRequests()
+                .antMatchers("/").permitAll()
+                .anyRequest().authenticated();
+        JwtFilter.registerFilter(http);
+    }
+
+}
