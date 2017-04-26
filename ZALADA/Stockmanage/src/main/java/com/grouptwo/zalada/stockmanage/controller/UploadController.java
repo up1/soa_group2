5879fefa7d52486d6dce5301e1facbcfe698a421
Thanarat Tasarika <s7070047@kmitl.ac.th>
2017-04-26 22:40:26 +0700

    manage library

--- ZALADA/Stockmanage/src/main/java/com/grouptwo/zalada/stockmanage/controller/UploadController.java
+++ ZALADA/Stockmanage/src/main/java/com/grouptwo/zalada/stockmanage/controller/UploadController.java
@@ -2,7 +2,6 @@ package com.grouptwo.zalada.stockmanage.controller;
 
 
 import com.grouptwo.zalada.stockmanage.exception.RepositoryException;
-import com.grouptwo.zalada.stockmanage.exception.RequestException;
 import com.grouptwo.zalada.stockmanage.exception.UploadException;
 import com.grouptwo.zalada.stockmanage.service.UploadService;
 import org.apache.commons.logging.Log;
@@ -13,7 +12,10 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 @Controller
