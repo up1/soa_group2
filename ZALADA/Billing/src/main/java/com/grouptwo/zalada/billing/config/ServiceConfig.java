59ec23866b42cef4872c4f5f1f5b30b555a7d0df
Jirateep Tatip <57070018@kmitl.ac.th>
2017-04-27 14:13:40 +0700

    remove method

--- ZALADA/Billing/src/main/java/com/grouptwo/zalada/billing/config/ServiceConfig.java
+++ ZALADA/Billing/src/main/java/com/grouptwo/zalada/billing/config/ServiceConfig.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
 import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
@@ -21,12 +20,7 @@ public class ServiceConfig extends AbstractMongoConfiguration {
 
     @Value("${spring.data.mongodb.database}")
     private String mongoDB;
-
-    @Override
-    public MongoMappingContext mongoMappingContext()
-            throws ClassNotFoundException {
-        return super.mongoMappingContext();
-    }
+    
     @Override
     @Bean
     public Mongo mongo() throws Exception {
