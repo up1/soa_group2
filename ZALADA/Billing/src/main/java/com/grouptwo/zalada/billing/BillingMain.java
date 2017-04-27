1915ce14a3a0a8cb1cb0db6662799423a2d94ab0
Jirateep Tatip <57070018@kmitl.ac.th>
2017-04-27 14:33:24 +0700

    add private Constructor

--- ZALADA/Billing/src/main/java/com/grouptwo/zalada/billing/BillingMain.java
+++ ZALADA/Billing/src/main/java/com/grouptwo/zalada/billing/BillingMain.java
@@ -6,8 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @EnableAutoConfiguration
-public class BillingMain {
+public class BillingMain{
 
+    private BillingMain() {
+        throw new IllegalAccessError("Utility class");
+    }
     public static void  main (String... args){
         SpringApplication.run(BillingMain.class, args);
     }
