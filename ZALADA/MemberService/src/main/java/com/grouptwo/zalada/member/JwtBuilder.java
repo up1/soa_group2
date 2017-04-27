f9652de3d5c81a929583dc3d67b4b7a8a9c728d0
Thanarat Tasarika <s7070047@kmitl.ac.th>
2017-04-27 23:23:12 +0700

    Create Build JWT Token Method

--- ZALADA/MemberService/src/main/java/com/grouptwo/zalada/member/JwtBuilder.java
+++ ZALADA/MemberService/src/main/java/com/grouptwo/zalada/member/JwtBuilder.java
@@ -4,9 +4,11 @@ import com.grouptwo.zalada.member.domain.Authenticated;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.security.core.Authentication;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Date;
 
 public class JwtBuilder {
 
@@ -14,6 +16,22 @@ public class JwtBuilder {
 
     private static final String SECRET_KEY = "pOnAm2017";
     private static final Long EXPIRATION_TIME = 600000L;
+
+    private JwtBuilder(){}
+
+    public static String build(String username){
+
+        Claims claims = Jwts.claims().setSubject(username);
+
+        Long now = System.currentTimeMillis();
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(new Date(now))
+                .setExpiration(new Date(now + EXPIRATION_TIME))
+                .signWith(SignatureAlgorithm.HS512, SECRET_KEY)
+                .compact();
+    }
+
     public static Authentication parse(HttpServletRequest request) {
         String token = request.getHeader("Authorization");
 
