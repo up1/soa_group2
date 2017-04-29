6640aaacc24ff9f8c1693bf5ef6d5dc20bf1d93c
Satit Sirihongthong <s7070127@kmitl.ac.th>
2017-04-29 19:58:13 +0700

    Unused import removed

--- ZALADA/MemberService/src/main/java/com/grouptwo/zalada/member/MemberManageMain.java
+++ ZALADA/MemberService/src/main/java/com/grouptwo/zalada/member/MemberManageMain.java
@@ -3,8 +3,6 @@ package com.grouptwo.zalada.member;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication
 @EnableAutoConfiguration
