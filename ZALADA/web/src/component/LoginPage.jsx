58727b5543f3541c13626792dd9c41fa122f023e
Leconize <g.supavit@gmail.com>
2017-04-29 18:44:48 +0700

    add login function

--- ZALADA/web/src/component/LoginPage.jsx
+++ ZALADA/web/src/component/LoginPage.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
+import cookie from 'react-cookie';
 import UpperHeaderSection from './Sale/UpperHeaderSection';
 import FooterSection from './Sale/FooterSection';
 import './css/loginPage.css';
 import logo from './img/logoZALADA.png';
-import axios from 'axios';
-import { UserService } from '../util/AxiosWrapper';
+import { MemberService } from '../util/AxiosWrapper';
 
 class LoginPage extends React.Component {
 
@@ -12,7 +12,7 @@ class LoginPage extends React.Component {
     super();
     this.state = {
       username: '',
-      password: ''
+      password: '',
     };
     this.handleChange = this
       .handleChange
@@ -29,9 +29,15 @@ class LoginPage extends React.Component {
   }
 
   handleSubmit() {
-    UserService
-      .post(`/member/signin?username=${this.state.username}&password=${this.state.password}`)
+    const data = {
+      ...this.state,
+    };
+    console.log(data);
+    MemberService
+      .post('/member/signin', data)
       .then((response) => {
+        console.log(response.data);
+        cookie.save('user', this.state.username);
         this
           .props
           .updateUser(this.state.username);
@@ -40,20 +46,20 @@ class LoginPage extends React.Component {
           .router
           .history
           .push('/');
-      },)
+      } )
       .catch((error) => {
         console.log(error);
-      },);
+      } );
   }
 
   render() {
     return (
       <div className="login-page">
-        <UpperHeaderSection/>
+        <UpperHeaderSection />
         <div className="container-login">
           <div id="login-box">
             <div className="logo">
-              <img src={logo} className="img img-responsive center-block" alt="logo"/>
+              <img src={logo} className="img img-responsive center-block" alt="logo" />
               <h1 className="logo-caption">เข้าสู่ระบบ</h1>
             </div>
             <div className="controls">
@@ -62,29 +68,32 @@ class LoginPage extends React.Component {
                 name="username"
                 placeholder="Username"
                 className="form-control username"
-                onChange={this.handleChange}/>
+                onChange={this.handleChange}
+              />
               <input
                 type="password"
                 name="password"
                 placeholder="Password"
                 className="form-control password"
-                onChange={this.handleChange}/>
+                onChange={this.handleChange}
+              />
               <button
                 type="button"
                 className="btn btn-default btn-block btn-custom"
                 id="btn-ok"
-                onClick={this.handleSubmit}>เข้าสู่ระบบ</button>
+                onClick={this.handleSubmit}
+              >เข้าสู่ระบบ</button>
             </div>
           </div>
         </div>
-        <FooterSection/>
+        <FooterSection />
       </div>
     );
   }
 }
 
 LoginPage.contextTypes = {
-  router: React.PropTypes.object
+  router: React.PropTypes.object,
 };
 
 export default LoginPage;
